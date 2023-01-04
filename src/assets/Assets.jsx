e15b7eb40837429aa61c85b6a054f339c6dcc0c5
Ricardo <ricardogf396@gmail.com>
2023-01-04 16:44:26 -0600

    Assets file created

---
+++ src/assets/Assets.jsx
@@ -0,0 +1,25 @@
+import bed from './bed.svg'
+import refri from './refrigerador.svg'
+import furniture from './forniture.svg'
+import oven from './oven.svg'
+import sofa from './sofa.svg'
+import tv from './tv.svg'
+import washer from './washer.svg'
+import dining from './dining.svg'
+import desk from './desk.svg'
+import wardrobe from './bed.svg'
+
+const images = {
+    bedImg: bed,
+    refriImg: refri,
+    furnitureImg: furniture,
+    ovenImg: oven,
+    sofaImg:sofa,
+    tvImg:tv,
+    washerImg: washer,
+    diningImg: dining,
+    deskImg: desk,
+    wardrobeImg: wardrobe,
+}
+
+export default images
