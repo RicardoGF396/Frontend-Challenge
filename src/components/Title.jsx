c06de43176dee86f7552be83ce4017405de2f912
Ricardo <ricardogf396@gmail.com>
2023-01-04 16:46:28 -0600

    Title Finished and Header was moved to components folder

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className='w-full flex justify-center items-center flex-col my-20 px-8'>
+        <h1 className='text-center text-5xl'>What items to store?</h1>
+        <p className='text-center mt-8 max-w-[450px] text-[#666666] '>
+        Select which items you wish to store before moving to your new home. We’ll keep ’em safe!
+        </p>
+    </div>
+  )
+}
+
+export default Title
